469cfb38d6e8ffa329cda4920ef214f9c696167c
SigridPihel <sigrid.pihel@yolo.com>
2024-12-06 10:05:21 +0200

    chapter 22 video code + power napper ex done

---
+++ lib/servy/power_napper.ex
@@ -0,0 +1,19 @@
+defmodule Servy.PowerNapper do
+  def power_nap() do
+
+    power_nap = fn ->
+      time = :rand.uniform(10_000)
+      :timer.sleep(time)
+      time
+    end
+
+    parent = self()
+
+    spawn(fn -> send(parent, {:slept, power_nap.()}) end)
+
+    receive do
+      {:slept, time} -> IO.puts "Slept #{time} ms"
+    end
+
+  end
+end
