819026fd90eb61386b720ee77fb3c67f756c411a
SigridPihel <sigrid.pihel@yolo.com>
2024-12-12 09:25:18 +0200

    use_httpoison_in_httpserver test done

--- test/http_server_test.exs
+++ test/http_server_test.exs
@@ -5,23 +5,12 @@ defmodule HttpServerTest do
   alias Servy.HttpClient
 
   test "accepts a request on a socket and sends back a response" do
-    request = """
-    GET /wildthings HTTP/1.1\r
-    Host: example.com\r
-    User-Agent: ExampleBrowser/1.0\r
-    Accept: */*\r
-    \r
-    """
-    spawn(fn -> Servy.HttpServer.start(4013) end)
-    response = HttpClient.send_request(request)
 
-    assert response == """
-    HTTP/1.1 200 OK\r
-    Content-Type: text/html\r
-    Content-Length: 20\r
-    \r
-    Bears, Lions, Tigers
-    """
+    spawn(HttpServer, :start, [4013])
+
+    {:ok, response} = HTTPoison.get "http://localhost:4013/wildthings"
+    assert response.status_code == 200
+    assert response.body == "Bears, Lions, Tigers"
   end
 
   defp remove_whitespace(text) do
