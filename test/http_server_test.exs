89a6d046e2447765472504a3c814701cfc166d6e
SigridPihel <sigrid.pihel@yolo.com>
2024-12-16 09:27:17 +0200

    chapter 22 done

--- test/http_server_test.exs
+++ test/http_server_test.exs
@@ -2,19 +2,32 @@ defmodule HttpServerTest do
   use ExUnit.Case
 
   alias Servy.HttpServer
-  alias Servy.HttpClient
 
   test "accepts a request on a socket and sends back a response" do
-
     spawn(HttpServer, :start, [4013])
 
-    {:ok, response} = HTTPoison.get "http://localhost:4013/wildthings"
-    assert response.status_code == 200
-    assert response.body == "Bears, Lions, Tigers"
-  end
+    parent = self()
 
-  defp remove_whitespace(text) do
-    String.replace(text, ~r{\s}, "")
-  end
+    max_concurrent_requests = 5
 
+    # Spawn the client processes
+    for _ <- 1..max_concurrent_requests do
+      spawn(fn ->
+        # Send the request
+        {:ok, response} = HTTPoison.get "http://localhost:4013/wildthings"
+
+        # Send the response back to the parent
+        send(parent, {:ok, response})
+      end)
+    end
+
+    # Await all {:handled, response} messages from spawned processes.
+    for _ <- 1..max_concurrent_requests do
+      receive do
+        {:ok, response} ->
+          assert response.status_code == 200
+          assert response.body == "Bears, Lions, Tigers"
+      end
+    end
+  end
 end
