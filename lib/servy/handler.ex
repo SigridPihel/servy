63c27772c83225669206f95bf2ad8eb4404c34ec
SigridPihel <sigrid.pihel@yolo.com>
2024-12-27 17:08:20 +0200

    chapter 23 except for exercises done

--- lib/servy/handler.ex
+++ lib/servy/handler.ex
@@ -25,14 +25,21 @@ defmodule Servy.Handler do
   end
 
   def route(%Conv{ method: "GET", path: "/sensors" } = conv) do
-    task = Task.async(fn -> Servy.Tracker.get_location("bigfoot") end)
+    task = Task.async(Servy.Tracker, :get_location, ["bigfoot"])
 
     snapshots =
       ["cam-1", "cam-2", "cam-3"]
       |> Enum.map(&Task.async(fn -> VideoCam.get_snapshot(&1) end))
       |> Enum.map(&Task.await/1)
 
-    where_is_bigfoot = Task.await(task)
+    where_is_bigfoot =
+      case Task.yield(task, :timer.seconds(5)) do
+        {:ok, result} ->
+          result
+        nil ->
+          Logger.warn "Timed out!"
+          Task.shutdown(task)
+      end
 
     %{ conv | status: 200, resp_body: inspect {snapshots, where_is_bigfoot}}
   end
