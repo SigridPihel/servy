e8b78b0d997a21053d4dcd258593aa25fec9a3b8
Sigrid Pihel <pihelsigrid@gmail.com>
2025-02-07 09:34:24 +0200

    chapter 28 exercise done

--- lib/servy/kick_starter.ex
+++ lib/servy/kick_starter.ex
@@ -1,15 +1,27 @@
 defmodule Servy.KickStarter do
   use GenServer
 
+  # Client Interface
+
   def start do
     IO.puts "Starting the kickstarter..."
     GenServer.start(__MODULE__, :ok, name: __MODULE__)
   end
 
+  def get_server do
+    GenServer.call __MODULE__, :get_server
+  end
+
+  # Server Callbacks
+
   def init(:ok) do
     Process.flag(:trap_exit, true)
     server_pid = start_server()
     {:ok, server_pid}
+  end
+
+  def handle_call(:get_server, _from, state) do
+    {:reply, state, state}
   end
 
   def handle_info({:EXIT, _pid, reason}, _state) do
@@ -20,8 +32,8 @@ defmodule Servy.KickStarter do
 
   defp start_server do
     IO.puts "Starting the HTTP server..."
-    server_pid = spawn_link(Servy.HttpServer, :start, [4014])
-    Process.register(server_pid, :http_server)
-    server_pid
+    server_pid = spawn_link(Servy.HttpServer, :start, [4016])
+    #Process.register(server_pid, :http_server)
+    #server_pid
   end
 end
